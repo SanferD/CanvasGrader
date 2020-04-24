7d3db3ab5d446214a72cedf89b9a4490e212e82a
Sanfer <dsouz039@umn.edu>
2020-04-24 01:35:21 -0500

    Add canvas grader readonly model inline

--- canvas_grader/admin/model_admins.py
+++ canvas_grader/admin/model_admins.py
@@ -8,3 +8,11 @@ class ROModelAdmin(InlineActionsModelAdminMixin, admin.ModelAdmin):
     def has_change_permission(self, request, obj = None):
         return False
 
+class ROModelInline(InlineActionsMixin, admin.TabularInline):
+    def has_add_permission(self, request, obj = None):
+        return False
+
+    def has_change_permission(self, request, obj = None):
+        return False
+    
+
