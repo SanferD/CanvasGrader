96bbc281c109a3c105737d18515f191188d8ccd6
Sanfer <dsouz039@umn.edu>
2020-04-25 11:44:02 -0500

    Add student dropdown menu

--- canvas_grader/static/grader/grader.js
+++ canvas_grader/static/grader/grader.js
@@ -4,11 +4,14 @@ function InitializeGrader($scope, $http)
 $scope.questions = GetValueJSON("questions")
 $scope.quiz_id = GetValue("quiz-id")
 $scope.canvas_users = GetValueJSON("canvas-users")
-console.log($scope.canvas_users)
 
-Initialize()
-function Initialize()
+$scope.canvas_user_current = $scope.canvas_users[0]
+$scope.canvas_user_selected = $scope.canvas_user_current
+
+$scope.ChangeCurrentCanvasUser = function()
 {
+    console.log($scope.canvas_user_selected)
 }
+
 
 }
