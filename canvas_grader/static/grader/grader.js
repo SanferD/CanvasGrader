7fcb08f6acd9ae23a4dd7d14e3e757401c67ace7
Sanfer <dsouz039@umn.edu>
2020-04-25 15:34:53 -0500

    Show submission + grader comments

--- canvas_grader/static/grader/grader.js
+++ canvas_grader/static/grader/grader.js
@@ -5,8 +5,10 @@ $scope.questions = GetValueJSON("questions")
 $scope.quiz_id = GetValue("quiz-id")
 $scope.canvas_users = GetValueJSON("canvas-users")
 
-$scope.canvas_user_current = $scope.canvas_users[0]
-$scope.canvas_user_selected = $scope.canvas_user_current
+$scope.canvas_user_current = undefined
+$scope.canvas_user_selected = $scope.canvas_users[0]
+
+$scope.submissions = []
 
 angular.element(Initialize)
 function Initialize()
@@ -16,11 +18,32 @@ function Initialize()
         q_div = document.getElementById(id)
         q_div.innerHTML = q.question_text.trim()
     })
+    $scope.ChangeCurrentCanvasUser()
 }
 
 $scope.ChangeCurrentCanvasUser = function()
 {
-    console.log($scope.canvas_user_selected)
+    console.log("change current user")
+    $scope.canvas_user_current = $scope.canvas_user_selected
+    $scope.GetSubmission($scope.canvas_user_current).then(function(resp) {
+        $scope.submissions = resp.data.submissions
+        $scope.submissions.forEach(ShowSubmission)
+    })
+}
+
+function ShowSubmission(submission)
+{
+    var id = "A" + submission.quiz_question_id
+    console.log(id)
+    a_div = document.getElementById(id)
+    if (a_div) {
+        a_div.innerHTML = submission.text
+
+        var assessment = submission.assessment
+        var id = "C" + submission.quiz_question_id
+        c_ta = document.getElementById(id)
+        c_ta.innerText = assessment? assessment.comment: ""
+    }
 }
 
 }
